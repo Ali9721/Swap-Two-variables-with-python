9b4fe3c2e979bfad7f8a20b1c7c51e20dc02281d
Ali Najafi <76534541+Ali9721@users.noreply.github.com>
2025-07-13 08:27:47 -0700

    Swap-Two-variables-with-python
    1. Define "a" & "b" as a variables .
    
    2. Define "a" & "b" to exchange each others.
    
    3. Output the result.

---
+++ Swap-Two-variables-with-python.py
@@ -0,0 +1,9 @@
+# Define "a" & "b" as a variables .
+a = int(input("Please enter the first number: "))
+b = int(input("Please enter the second number: "))
+
+# Define "a" & "b" to exchange each others.
+a, b = b, a
+
+# Output the result.
+print(f"After swapping: a = {a}, b = {b}")
